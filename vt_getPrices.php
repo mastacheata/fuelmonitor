3e2cd8226680106b76ca09a8c6396eac1de0876c
Benedikt Bauer <benedikt@xenzilla.de>
2016-05-09 20:18:29 +0200

    Remove Cron log in favor of Sentry logging

--- vt_getPrices.php
+++ vt_getPrices.php
@@ -7,8 +7,6 @@
 
 require 'vendor/autoload.php';
 
-file_put_contents(dirname(dirname($_SERVER['SCRIPT_FILENAME'])).'/log/cron.log', date('Y-m-d H:i:s').' Script executed'."\n", FILE_APPEND);
-
 $fm = new \Xenzilla\FuelMonitor\VollerTankMonitor();
 $fm->setCache();
 $fm->setPreferences();
